1ae81e9039f5f1c554afa112c6c9a2b5b7fb4845
siphumelele Dlamini <dlaminisiphi3@gmail.com>
2023-11-23 17:31:36 +0200

    Complete signup and login page designs

--- website/user.py
+++ website/user.py
@@ -2,7 +2,20 @@ from flask import Blueprint, render_template
 
 user = Blueprint('user',__name__)
 
+# Route for the home page
 @user.route('/')
 def home():
+    # Render the home page template
     return render_template("home_page.html")
 
+# Route for the login page
+@user.route('/login', methods=['GET', 'POST'])
+def login():
+    # Render the login page template
+    return render_template("login_page.html")
+
+# Route for the signup page
+@user.route('/signup', methods=['GET', 'POST'])
+def signup():
+    # Render the signup page template
+    return render_template("signup_page.html")
